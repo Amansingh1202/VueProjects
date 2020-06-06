f5ab55340a6767ab63f21d0b98ba6f6e95075cbd
Amansingh1202 <Aks001235@gmail.com>
2020-06-06 10:57:36 +0530

    add new project

---
+++ vue-music/src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="header">
+        Hello World
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Header',
+        props: {
+        }
+    }
+</script>
+
+<style scoped>
+</style>
