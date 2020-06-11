92d1f3acad94c535135365ef493c4af1eec832ab
Amansingh1202 <Aks001235@gmail.com>
2020-06-11 12:26:28 +0530

    complete about component

--- website-template/src/components/About.vue
+++ website-template/src/components/About.vue
@@ -1,5 +1,14 @@
 <template>
-    <h1>ABOUT</h1>
+    <div id="about">
+        <h1>- Our Mission -</h1>
+        <h2>To create website that look and feel beautiful</h2>
+        <h1 id="about-header">About</h1>
+        <p>
+            Lorem ipsum dolor sit amet, pede ac. Nec leo suspendisse, duis ut nunc aenean ante, inceptos in, pellentesque purus ornare lorem tincidunt non, varius duis elit. Conubia cras eget suspendisse convallis dui fuga, mi aliquam vestibulum. Nec nunc viverra morbi et turpis, morbi tristique aenean praesent ipsum vulputate, sem vel. Leo eget, vestibulum ac debitis in eros diam, leo elit tortor mollis laoreet varius, consectetuer in tempus lectus, rutrum vitae nec eu senectus ut adipiscing. Mollis quam mi leo tellus vestibulum, ad ut sed.
+
+            Id volutpat sodales tellus rutrum id, vitae fermentum diam interdum erat neque in, urna integer tristique libero ac in et, et phasellus vulputate sit. Ut gravida, tortor ut arcu, dui auctor, purus posuere integer. Cras quis ac porttitor sem vel id. Justo morbi scelerisque nec turpis sit, faucibus non vestibulum ipsum metus vitae. Sit potenti, vitae imperdiet neque est ut nunc, erat feugiat mauris enim elit. Quis porttitor taciti non, non aliquip libero volutpat quisque amet. Phasellus vivamus quisque convallis, vestibulum hac ut, mattis et, augue luctus neque.
+        </p>
+    </div>
 </template>
 
 <script>
@@ -10,4 +19,26 @@
 
 <style scoped>
 
+    h1[data-v-c226fde6]{
+        color: gray;
+        font-size:2em;
+        margin-left:44%;
+    }
+    #about[data-v-c226fde6]{
+        background-color: whitesmoke;
+        padding: 2%;
+    }
+    #about h2{
+        font-size: 1.6em;
+        margin: 20px 0 20px 36%;
+    }
+    #about-header{
+        margin-left:47%;
+    }
+    p{
+        font-size:1.2em;
+        width: 80%;
+        margin-left: 10%;
+        text-align: justify;
+    }
 </style>
